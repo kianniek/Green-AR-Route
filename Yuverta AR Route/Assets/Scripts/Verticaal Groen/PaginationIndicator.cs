0eaf2e0b8325e56102c1d52393c33fdb04c11df8
Tristan <157964375+TheGamingT-rex@users.noreply.github.com>
2024-06-14 17:07:17 +0200

    Added tutorial

---
+++ Yuverta AR Route/Assets/Scripts/Verticaal Groen/PaginationIndicator.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class PaginationIndicator : MonoBehaviour
+{
+    public Image dot;
+    public Color activeColor;
+    public Color inactiveColor;
+}
