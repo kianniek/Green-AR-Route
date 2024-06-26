9d43c1905a7d29aeffd54ecff278b38484cc9311
Tristan <157964375+TheGamingT-rex@users.noreply.github.com>
2024-06-26 16:25:03 +0200

    Attempted inputs

--- Yuverta AR Route/Assets/Scripts/Stadslandbouw/CropContainer.cs
+++ Yuverta AR Route/Assets/Scripts/Stadslandbouw/CropContainer.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 
 [RequireComponent(typeof(CropTracker), typeof(CropGrowthSystem))]
@@ -20,18 +23,9 @@ public class CropContainer : MonoBehaviour
     public UnityEvent onCropHarvested;
     public UnityEvent onCropPlanted;
     
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private InputActionProperty dragDeltaAction;
+    private bool hitCrop;
+    private bool newCrop;
 
     public void NewCrop(CropScript newCropObject)
     {
@@ -50,13 +44,90 @@ public class CropContainer : MonoBehaviour
         onCropPlanted.Invoke();
         
         onCropHarvested.AddListener(newCropObject.HarvestCrop);
+        
+        dragDeltaAction.action.Enable();
+        newCrop = true;
     }
     
     public void HarvestCrop()
     {
-        if (currentCropObject.growthStage == currentCropObject.cropObject.growthStages.Count - 1)
+        if (currentCropObject.growthStage != currentCropObject.cropObject.growthStages.Count - 1) return;
+        
+        onCropHarvested.Invoke();
+        dragDeltaAction.action.Disable();
+    }
+    
+    private void Update()
+    {
+        if (dragDeltaAction.action != null && dragDeltaAction.action.triggered)
         {
-            onCropHarvested.Invoke();
+            Vector2 touchPosition = dragDeltaAction.action.ReadValue<Vector2>();
+
+            if (Touchscreen.current.primaryTouch.press.isPressed)
+            {
+                var eventData = new PointerEventData(EventSystem.current)
+                {
+                    position = touchPosition
+                };
+                OnPointerDown(eventData);
+            }
+            else if (Touchscreen.current.primaryTouch.phase.ReadValue() == UnityEngine.InputSystem.TouchPhase.Moved)
+            {
+                var eventData = new PointerEventData(EventSystem.current)
+                {
+                    position = touchPosition
+                };
+                OnDrag(eventData);
+            }
+            else if (Touchscreen.current.primaryTouch.phase.ReadValue() == UnityEngine.InputSystem.TouchPhase.Ended)
+            {
+                var eventData = new PointerEventData(EventSystem.current)
+                {
+                    position = touchPosition
+                };
+                OnEndDrag(eventData);
+                OnPointerUp(eventData);
+            }
         }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Send a ray from the touch position into the world to check if it collides with objects
+        Ray ray = Camera.main.ScreenPointToRay(eventData.position);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            //If the ray hits a crop, set the drag object to the crop
+            if (hit.collider.CompareTag("Crop"))
+            {
+                hitCrop = true;
+                
+                if (!newCrop)
+                {
+                    FindObjectOfType<CropTracker>().PickedSeed(hit.collider.name);
+                }
+            }
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (hitCrop)
+        {
+            HarvestCrop();
+        }
+        
+        newCrop = false;
+        hitCrop = false;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        
     }
 }
