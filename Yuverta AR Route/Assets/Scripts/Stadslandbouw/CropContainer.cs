583e10b414205daa1517546139febf3c8e7f6680
HVA-Tristan <157964375+TheGamingT-rex@users.noreply.github.com>
2024-06-27 14:55:57 +0200

    Added CropType, Animator, Buttons and comments

--- Yuverta AR Route/Assets/Scripts/Stadslandbouw/CropContainer.cs
+++ Yuverta AR Route/Assets/Scripts/Stadslandbouw/CropContainer.cs
@@ -1,48 +1,49 @@
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.Serialization;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+// ReSharper disable CollectionNeverUpdated.Global
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnassignedField.Global
 
 [RequireComponent(typeof(CropTracker), typeof(CropGrowthSystem))]
 public class CropContainer : MonoBehaviour
 {
     [Tooltip("This list contains the spawn locations for the seeds the crop tracker spawns")]
     public List<Transform> seedsSpawnLocations; 
+    [Tooltip("This is the location where the crop will spawn when the seed is planted.")]
     public Transform cropSpawnLocation;
+    [Tooltip("This is the text that will display the name of the crop.")]
     [SerializeField] private TextMeshPro cropNameDisplay;
     public CropScript currentCropObject;
     private CropScript lastCorrectCropObject;
-    private CropScript lastCropObject;
+    
+    /// <summary>
+    /// This bool is to store if the crop is correct or not.
+    /// </summary>
     private bool rightCrop;
     
     public UnityEvent onCropHarvested;
     public UnityEvent onCropPlanted;
-    
+
+    public InputActionReference harvestTap;
     
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        onCropHarvested = new UnityEvent();
+        onCropPlanted = new UnityEvent();
+        harvestTap.action.performed += _ => HarvestCrop();
     }
 
     public void NewCrop(CropScript newCropObject)
     {
-        if (rightCrop)
-        {
-            lastCorrectCropObject = currentCropObject;
-        }
-        else lastCropObject = currentCropObject;
+        if (rightCrop) lastCorrectCropObject = currentCropObject;
         
         currentCropObject = newCropObject;
-        rightCrop = lastCorrectCropObject.cropObject.nextCrop == currentCropObject.cropObject.cropName;
+        rightCrop = lastCorrectCropObject.cropObject.nextCrop == currentCropObject.cropObject.cropType;
         
         //Spawning the first stage of the crop
         newCropObject.GrowCrop();
@@ -50,13 +51,22 @@ public class CropContainer : MonoBehaviour
         onCropPlanted.Invoke();
         
         onCropHarvested.AddListener(newCropObject.HarvestCrop);
+        newCropObject.fullyGrown.AddListener(harvestTap.action.Enable);
     }
     
-    public void HarvestCrop()
+    private void HarvestCrop()
     {
-        if (currentCropObject.growthStage == currentCropObject.cropObject.growthStages.Count - 1)
+        if (currentCropObject.growthStage != currentCropObject.cropObject.growthStages.Count - 1) return;
+        
+        //Check with a rayCast if the touch actually hit the crop
+        var pointerReference = new PointerEventData(EventSystem.current);
+        var ray = Camera.main.ScreenPointToRay(pointerReference.position);
+        if (Physics.Raycast(ray, out var hit, 1000f))
         {
-            onCropHarvested.Invoke();
+            if (hit.collider.gameObject != currentCropObject.gameObject) return;
         }
+        
+        onCropHarvested.Invoke();
+        harvestTap.action.Disable();
     }
 }
